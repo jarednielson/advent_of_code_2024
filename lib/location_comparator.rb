b9cdc40cab622ccfe5471cd88aaf2644fefe0c26
jarednielson <jarednielsonn@gmail.com>
2025-02-07 22:18:11 -0700

    ref: Prefer guard clause

--- lib/location_comparator.rb
+++ lib/location_comparator.rb
@@ -9,24 +9,23 @@ class LocationComparator
   end
 
   def total_distance
-    if @total_distance.nil?
-      left.zip(right).reduce(0) do |total_distance, (left_location, right_location)|
-        total_distance += (left_location - right_location).abs
-      end
+    return @total_distance unless @total_distance.nil?
+
+    @total_distance = left.zip(right).reduce(0) do |total_distance, (left_location, right_location)|
+      total_distance += (left_location - right_location).abs
     end
   end
 
   def similarity_score
-    if @similarity_score.nil?
-      right_occurence_counts = right.reduce(Hash.new(0)) do |accum, right_location|
-        accum[right_location] += 1
-        accum
-      end
+    return @similarity_score unless @similarity_score.nil?
 
-      @similarity_score = left.reduce(0) do |running_score, left_location|
-        running_score + left_location * right_occurence_counts[left_location]
-      end
+    right_occurence_counts = right.reduce(Hash.new(0)) do |accum, right_location|
+      accum[right_location] += 1
+      accum
     end
-    @similarity_score
+
+    @similarity_score = left.reduce(0) do |running_score, left_location|
+      running_score + left_location * right_occurence_counts[left_location]
+    end
   end
 end
