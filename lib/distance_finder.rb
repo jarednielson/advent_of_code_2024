9e93fa882c471f384c2bea21c39a24b15957ce36
jarednielson <jarednielsonn@gmail.com>
2025-02-07 21:41:57 -0700

    Ensure lists are same length on init

--- lib/distance_finder.rb
+++ lib/distance_finder.rb
@@ -1,7 +1,9 @@
 class DistanceFinder
+  class ListLengthMismatch < StandardError; end
   attr_reader :left, :right
 
   def initialize(left:, right:)
+    raise ListLengthMismatch.new "Lists must be the same length. Left length: #{left.length} -- Right length: #{right.length}" if left.length != right.length
     @left = left.sort
     @right = right.sort
   end
--- spec/lib/distance_finder_spec.rb
+++ spec/lib/distance_finder_spec.rb
@@ -8,4 +8,12 @@ describe DistanceFinder do
     expect(finder.left).to eq [1, 2, 3]
     expect(finder.right).to eq [1, 2, 3]
   end
+
+  context "when left and right have mismatched lengths" do
+    it "raises a helpful error" do
+      expect {
+        DistanceFinder.new(left: [1, 2], right: [1, 2, 3])
+      }.to raise_error DistanceFinder::ListLengthMismatch
+    end
+  end
 end
