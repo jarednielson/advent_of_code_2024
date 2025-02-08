da2ca8cfed4d6359502af4d5064cda7316fc97a3
jarednielson <jarednielsonn@gmail.com>
2025-02-07 23:30:08 -0700

    Add method safe?

--- lib/report.rb
+++ lib/report.rb
@@ -1,7 +1,32 @@
 class Report
   attr_reader :levels
-  
+
   def initialize(levels)
     @levels = levels
+  end
+
+  def safe?
+    return @safe unless @safe.nil?
+
+    all_close = levels.each_cons(2).all? do |first, second|
+      (first - second).abs.between?(1, 3)
+    end
+
+    @safe = all_close && (all_decreasing || all_increasing)
+  end
+
+
+  private
+
+  def all_decreasing
+    levels.each_cons(2).all? do |first, second|
+      first < second
+    end
+  end
+
+  def all_increasing
+    levels.each_cons(2).all? do |first, second|
+      first > second
+    end
   end
 end
--- spec/lib/report_spec.rb
+++ spec/lib/report_spec.rb
@@ -5,4 +5,13 @@ describe Report do
   it "is initialized with a list" do
     expect(Report.new([1, 2, 3, 4]).levels).to eq [1, 2, 3, 4]
   end
+
+  it "determines safety" do
+    expect(Report.new([7, 6, 4, 2, 1]).safe?).to eq true
+    expect(Report.new([1, 2, 7, 8, 9]).safe?).to eq false
+    expect(Report.new([9, 7, 6, 2, 1]).safe?).to eq false
+    expect(Report.new([1, 3, 2, 4, 5]).safe?).to eq false
+    expect(Report.new([8, 6, 4, 4, 1]).safe?).to eq false
+    expect(Report.new([1, 3, 6, 7, 9]).safe?).to eq true
+  end
 end
