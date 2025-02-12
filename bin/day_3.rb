7a10aacd03628ef20668b408d0d4f532849ea097
jarednielson <jarednielsonn@gmail.com>
2025-02-12 00:24:25 -0700

    Day 3 part 2

--- bin/day_3.rb
+++ bin/day_3.rb
@@ -1,12 +1,17 @@
 #! ruby
 
+$LOAD_PATH.unshift(File.expand_path('..', File.dirname(__FILE__)))
+
 require_relative '../lib/instruction_cleaner'
-require_relative '../lib/mul_instruction'
+require_relative '../lib/instruction_parser'
+require_relative '../lib/interpreter'
+
 
 memory = File.read(File.expand_path("../input/day_3", File.dirname(__FILE__)))
 
-sum = InstructionCleaner.new(raw_memory: memory).instructions.reduce(0) do |accum, instruction|
-  accum += MulInstruction.new(instruction: instruction).interp
-end
+raw_instructions = InstructionCleaner.new(raw_memory: memory).instructions
+instructions = InstructionParser.instructions(raw_instructions: raw_instructions)
 
-puts "Sum: #{sum}"
+result = Interpreter.new(instructions: instructions).result
+
+puts "Sum: #{result}"
