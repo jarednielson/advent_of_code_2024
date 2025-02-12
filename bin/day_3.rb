55028f35740fc905fd1faa0d296e980eed46fd00
jarednielson <jarednielsonn@gmail.com>
2025-02-11 23:09:46 -0700

    Day 3

---
+++ bin/day_3.rb
@@ -0,0 +1,12 @@
+#! ruby
+
+require_relative '../lib/instruction_cleaner'
+require_relative '../lib/mul_instruction'
+
+memory = File.read(File.expand_path("../input/day_3", File.dirname(__FILE__)))
+
+sum = InstructionCleaner.new(raw_memory: memory).instructions.reduce(0) do |accum, instruction|
+  accum += MulInstruction.new(instruction: instruction).eval
+end
+
+puts "Sum: #{sum}"
