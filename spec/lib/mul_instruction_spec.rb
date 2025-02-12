b77f365b0a0df605bb4f1cea4f09d00ab2779d99
jarednielson <jarednielsonn@gmail.com>
2025-02-11 23:21:54 -0700

    Backfill specs

--- spec/lib/mul_instruction_spec.rb
+++ spec/lib/mul_instruction_spec.rb
@@ -4,18 +4,30 @@ require 'spec_helper'
 require 'lib/mul_instruction'
 
 describe MulInstruction do
+  subject(:mul_instruction) { MulInstruction.new(instruction: instruction) }
   context "when the instruction is properly formatted" do
     let(:instruction) { "mul(20,31)" }
+
     it "can read and evaluate the instruction" do
-      expect(MulInstruction.new(instruction: instruction).eval).to eq 20 * 31
+      expect(mul_instruction.eval).to eq 20 * 31
     end
 
     it "has an l_operand" do
-      expect(MulInstruction.new(instruction: instruction).l_operand).to eq 20
+      expect(mul_instruction.l_operand).to eq 20
     end
 
     it "has an r_operand" do
-      expect(MulInstruction.new(instruction: instruction).r_operand).to eq 31
+      expect(mul_instruction.r_operand).to eq 31
+    end
+  end
+
+  context "when the instruction is not properly formatted" do
+    let(:instruction) { "poopoopeepee" }
+
+    it "raises a helpful error" do
+      expect do
+        subject
+      end.to raise_error MulInstruction::MalformedInstruction
     end
   end
 end
