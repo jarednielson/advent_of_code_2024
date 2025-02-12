7306691974ef3d3969aeb56712ecc2110b033b3b
jarednielson <jarednielsonn@gmail.com>
2025-02-11 23:15:46 -0700

    Handle dos and donts when cleaning

--- lib/instruction_cleaner.rb
+++ lib/instruction_cleaner.rb
@@ -4,6 +4,10 @@ class InstructionCleaner
   attr_reader :raw_memory
 
   MUL_INSTR_PATTERN = /mul\(\d+,\d+\)/
+  DO_INSTR_PATTERN = /do\(\)/
+  DONT_INSTR_PATTERN = /don't\(\)/
+
+  INSTR_PATTERN = /#{MUL_INSTR_PATTERN}|#{DO_INSTR_PATTERN}|#{DONT_INSTR_PATTERN}/
 
   def initialize(raw_memory:)
     @raw_memory = raw_memory
@@ -11,6 +15,6 @@ class InstructionCleaner
 
   def instructions
     return @instructions unless @instructions.nil?
-    @instructions = raw_memory.scan(MUL_INSTR_PATTERN)
+    @instructions = raw_memory.scan(INSTR_PATTERN)
   end
 end
--- spec/lib/instruction_cleaner_spec.rb
+++ spec/lib/instruction_cleaner_spec.rb
@@ -9,4 +9,10 @@ describe InstructionCleaner do
       raw_memory: "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
     ).instructions).to eq %w[mul(2,4) mul(5,5) mul(11,8) mul(8,5)]
   end
+
+  it "handles do's and dont's" do
+    expect(InstructionCleaner.new(
+      raw_memory: "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    ).instructions).to eq %w[mul(2,4) don't() mul(5,5) mul(11,8) do() mul(8,5)]
+  end
 end
