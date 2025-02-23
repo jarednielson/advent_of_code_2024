906502e53884397871ba11a6ed0e164553bf2520
jarednielson <jarednielsonn@gmail.com>
2025-02-22 17:09:09 -0700

    Prefer subject syntax

--- spec/lib/dependency_graph_spec.rb
+++ spec/lib/dependency_graph_spec.rb
@@ -5,10 +5,9 @@ require 'lib/dependency_graph'
 
 
 describe DependencyGraph do
-  it "returns all dependents for a node" do
-    dependencies = ["47|53", "47|20"]
-    graph = DependencyGraph.new(dependencies: dependencies)
+  describe "#dependents" do
+    subject { DependencyGraph.new(dependencies: %w[47|53 47|20]).dependents("47") }
 
-    expect(graph.dependents("47")).to eq ["53", "20"].to_set
+    it { is_expected.to eq ["53", "20"].to_set }
   end
 end
