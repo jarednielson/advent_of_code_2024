82a3d57e88bf600693176ae2dbf5aa4473d4abfd
jarednielson <jarednielsonn@gmail.com>
2025-02-22 17:27:24 -0700

    Add descendant?

--- lib/dependency_graph.rb
+++ lib/dependency_graph.rb
@@ -15,7 +15,16 @@ class DependencyGraph
 
   def dependents(value)
     return [] unless graph.key?(value)
-    graph[value].dependents.map(&:value).to_set
+    graph[value].dependents.values.map(&:value).to_set
+  end
+
+  def descendant?(ancestor:, descendant:)
+    return false unless graph.key?(ancestor) && graph.key?(descendant)
+
+    ancestor = graph[ancestor]
+    descendant = graph[descendant]
+
+    !ancestor.dig(descendant).nil?
   end
 
   private
@@ -27,11 +36,22 @@ class DependencyGraph
 
     def initialize(value:)
       @value = value
-      @dependents = Set.new
+      @dependents = {}
     end
 
     def add_dependent(dependent:)
-      dependents << dependent
+      dependents[dependent.value] = dependent
+    end
+
+    def dig(node)
+      return dependents[node.value] if dependents.key?(node.value)
+
+      dependents.each do |_key, val|
+        result = val.dig(node)
+        return result unless result.nil?
+      end
+
+      return nil
     end
   end
 end
--- spec/lib/dependency_graph_spec.rb
+++ spec/lib/dependency_graph_spec.rb
@@ -10,4 +10,26 @@ describe DependencyGraph do
 
     it { is_expected.to eq ["53", "20"].to_set }
   end
+
+  describe "descendant?" do
+    let(:graph) { DependencyGraph.new(dependencies: %w[47|53 20|60 53|20]) }
+
+    context "when the value is a descendent" do
+      subject { graph.descendant?(ancestor: "47", descendant: "60") }
+
+      it { is_expected.to be true }
+    end
+
+    context "when the value is not a descendant" do
+      subject { graph.descendant?(ancestor: "20", descendant: "53") }
+
+      it { is_expected.to be false }
+    end
+
+    context "when the either value is not in the graph" do
+      subject { graph.descendant?(ancestor: "32", descendant: "61") }
+
+      it { is_expected.to be false }
+    end
+  end
 end
