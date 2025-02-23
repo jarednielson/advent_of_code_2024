c7aa5cd32c0eafc59dd73258ebbe1683d928866b
jarednielson <jarednielsonn@gmail.com>
2025-02-23 11:32:18 -0700

    Add spec

--- spec/lib/order_verifier_spec.rb
+++ spec/lib/order_verifier_spec.rb
@@ -19,11 +19,23 @@ describe OrderVerifier do
     %w[75 47 61 53 29],
     %w[97 61 53 29 13],
     %w[75 29 13]
-  ].each do |order|
-    context "when passing a valid order #{order}" do
-      subject { verifier.valid_order?(order: order) }
+  ].each do |valid_order|
+    context "when passing a valid order #{valid_order}" do
+      subject { verifier.valid_order?(order: valid_order) }
 
       it { is_expected.to be true }
+    end
+  end
+
+  [
+    %w[75 97 47 61 53],
+    %w[61 13 150 29],
+    %w[97 13 75 29 47]
+  ].each do |invalid_order|
+    context "when passing an invalid order #{invalid_order}" do
+      subject { verifier.valid_order?(order: invalid_order) }
+
+      it { is_expected.to be false }
     end
   end
 end
